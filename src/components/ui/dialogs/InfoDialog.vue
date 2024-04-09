d0c5b2c435f7c750a378e07dcb34bda8fc973c8e
Elvis Silva <elvis@computech.ie>
2024-04-09 15:25:02 +0100

    Moving InfoDialog to component

---
+++ src/components/ui/dialogs/InfoDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <Dialog
+    v-model:visible="localDisplayInfoDialog"
+    modal
+    :style="{ width: '25rem' }"
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+  >
+    <template #header="">
+      <h4>{{ infoModalHeader }}</h4>
+    </template>
+
+    <span class="font-bold text-2xl block mb-2 mt-4"></span>
+    <p class="mb-0">{{ infoModalMessage }}</p>
+    <div class="flex text-end gap-2 mt-4">
+      <Button label="Ok" @click="closeInfo"></Button>
+    </div>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+
+export default {
+  props: [
+    'displayInfoDialog',
+    'infoModalHeader',
+    'infoModalMessage',
+    'closeInfo'
+  ],
+  components: {
+    Dialog,
+    Button,
+  },
+  data() {
+    return {
+      localDisplayInfoDialog: this.displayInfoDialog,
+    }
+  },
+  watch: {
+    displayInfoDialog(newValue) {
+      this.localDisplayInfoDialog = newValue;
+    },
+  },
+}
+</script>
+
+<style></style>
