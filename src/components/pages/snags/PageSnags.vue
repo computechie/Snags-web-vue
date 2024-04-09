5d2d8a679b7360873a17043e6a692113b60130c4
Elvis Silva <elvis@computech.ie>
2024-04-09 08:43:47 +0100

    Moving RightSidePanel to component

--- src/components/pages/snags/PageSnags.vue
+++ src/components/pages/snags/PageSnags.vue
@@ -592,231 +592,19 @@
     Snags not found!
   </div>
 
-  <div id="RightSidepanel" class="RightSidepanel" :class="panelClass">
-    <div class="row">
-      <div class="col-md-12 text-end">
-        <i
-          class="pi pi-times cursorHand closePanelButton"
-          @click="closePanel"
-        ></i>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col text-center">
-        <h5 class="detailsHeadlineCode">{{ snagDetailsData.code }}</h5>
-        <h5 class="detailsHeadline">{{ snagDetailsData.caption }}</h5>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col text-center">
-        <div :class="'statusBlock status' + snagDetailsData.status">
-          {{ snagDetailsData.status }}
-        </div>
-      </div>
-    </div>
-
-    <div class="text-end" id="editIconsPanel">
-      <div class="actionButtonsHolder">
-        <div
-          class="fa-stack fa-2x actionIcons"
-          style="font-size: 15px"
-          v-tooltip.bottom="{
-            value: 'Edit snag',
-            showDelay: 1000,
-            hideDelay: 300,
-          }"
-          @click="editClick"
-        >
-          <i class="fa-solid fa-square fa-stack-2x"></i>
-          <i
-            class="fa-stack-1x fa-solid fa-pen-to-square"
-            style="color: white"
-          ></i>
-        </div>
-
-        <div
-          class="fa-stack fa-2x actionIcons"
-          style="font-size: 15px; color: #aa0000"
-          v-tooltip.left="{
-            value: 'Delete snag',
-            showDelay: 1000,
-            hideDelay: 300,
-          }"
-          @click="deleteClick"
-        >
-          <i class="fa-solid fa-square fa-stack-2x"></i>
-          <i class="fa-stack-1x fa-solid fa-trash-can" style="color: white"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="row" style="">
-      <div class="col text-center" style="padding: 19px">
-        <TabView v-model:activeIndex="activeTab">
-          <TabPanel header="Details">
-            <div class="row" style="margin-bottom: 10px">
-              <div class="col text-start" style="padding: 0">
-                <Button
-                  v-tooltip.bottom="{
-                    value: 'Download PDF',
-                    showDelay: 1000,
-                    hideDelay: 300,
-                  }"
-                  type="button"
-                  icon="pi pi-file-pdf"
-                  class="p-button-text donwloadPDFbutton"
-                  @click="downloadSnagPDF"
-                  label="Download PDF"
-                />
-              </div>
-            </div>
-
-            <table class="table table-striped table-sm" id="detailsTable">
-              <tbody>
-                <tr>
-                  <td style="width: 160px">Date Opened:</td>
-                  <td>{{ snagDetailsData.date }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 160px">Action by:</td>
-                  <td>{{ snagDetailsData.assignedTo }}</td>
-                </tr>
-
-                <tr>
-                  <td>Location:</td>
-                  <td>{{ snagDetailsData.area }}</td>
-                </tr>
-
-                <tr>
-                  <td>Works Package:</td>
-                  <td>{{ snagDetailsData.worksPackage }}</td>
-                </tr>
-
-                <tr>
-                  <td>Owner:</td>
-                  <td>{{ snagDetailsData.createdBy }}</td>
-                </tr>
-
-                <tr>
-                  <td>Closed by:</td>
-                  <td>{{ snagDetailsData.closedBy }}</td>
-                </tr>
-
-                <tr>
-                  <td>Notes:</td>
-                  <td>
-                    <textarea
-                      rows="5"
-                      v-text="snagDetailsData.notes"
-                      style="width: 100%"
-                      readonly
-                    ></textarea>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <div class="row">
-              <div class="col-md-6 text-left" style="padding: 0"></div>
-
-              <div class="col-md-6 text-end" style="padding: 0">
-                <Button
-                  @click="openDialogAddPhoto"
-                  style="min-width: 120px"
-                  v-tooltip.bottom="{
-                    value: 'Add photo',
-                    showDelay: 1000,
-                    hideDelay: 300,
-                  }"
-                  type="button"
-                  class="p-button-text addphotobutton"
-                  label="Add photo"
-                />
-              </div>
-            </div>
-          </TabPanel>
-
-          <TabPanel
-            :header="'Pictures (' + snagImages.length + ')'"
-            v-if="picturesTabShow"
-          >
-            <SwiperCarousel
-              :notes="snagNotes"
-              :tripImages="snagImages"
-              @showloader="showloader"
-              :openedcargoname="openedcargoname"
-              @swiper="onSwiper"
-              @deletePhoto="deletePhotoId"
-              @deletePhotomodal="deletePhotoModal"
-              :userType="userType"
-            />
-
-            <div class="row" style="margin-top: 12px">
-              <div class="col text-end" style="padding: 0">
-                <Button
-                  @click="openDialogAddPhoto"
-                  style="min-width: 120px"
-                  v-tooltip.bottom="{
-                    value: 'Add photo',
-                    showDelay: 1000,
-                    hideDelay: 300,
-                  }"
-                  type="button"
-                  class="p-button-text addphotobutton"
-                  label="Add photo"
-                />
-              </div>
-            </div>
-          </TabPanel>
-
-          <TabPanel header="Drawing">
-            <img src="/images/pindrawing.jpg" />
-          </TabPanel>
-        </TabView>
-      </div>
-    </div>
-
-    <div class="historyHolder">
-      <div class="row">
-        <div class="col-md-12">
-          <Timeline :value="events" align="alternate">
-            <template #marker="slotProps">
-              <span
-                class="flex w-2rem h-2rem align-items-center justify-content-center text-white border-circle z-1 shadow-1"
-                :style="{ backgroundColor: slotProps.item.color }"
-              >
-                <i :class="slotProps.item.icon"></i>
-              </span>
-            </template>
-
-            <template #content="slotProps">
-              <div class="mt-3">
-                <div class="historyStatus">{{ slotProps.item.status }}</div>
-                <div class="historyDate">{{ slotProps.item.date }}</div>
-                <div class="historyName">{{ slotProps.item.createdBy }}</div>
-                <div class="historyNotes">
-                  <textarea class="textAreaComments">Comments</textarea>
-                </div>
-              </div>
-            </template>
-          </Timeline>
-        </div>
-      </div>
-    </div>
-    <!-- zoomed image Dialog-->
-    <Dialog
-      modal
-      v-model:visible="visibleImagesModal"
-      :style="{ width: '50rem' }"
-      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
-    >
-      <SwiperCarousel
-        :snagImages="snagImages"
-        @deletePhotomodal="deletePhotoModal"
-      />
-    </Dialog>
-  </div>
+  <RightSidePanel
+    :panelClass="panelClass"
+    :snagDetailsData="snagDetailsData"
+    :activeTab="activeTab"
+    :events="events"
+    :visibleImagesModal="visibleImagesModal"
+    :closedPanel="closedPanel"
+    :snagImages="snagImages"
+    :deletePhotoModal="deletePhotoModal"
+    :picturesTabShow="picturesTabShow"
+    :downloadSnagPDF="downloadSnagPDF"
+    :openDialogAddPhoto="openDialogAddPhoto"
+  />
 
   <!-- insert new trip Dialog-->
   <Dialog
@@ -1362,8 +1150,9 @@
 </template>
 
 <script>
-// Primevue datatable help: https://www.primefaces.org/primevue/datatable //
+import RightSidePanel from "../../ui/RightSidePanel.vue";
 
+// Primevue datatable help: https://www.primefaces.org/primevue/datatable //
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import InputText from "primevue/inputtext";
@@ -1372,12 +1161,9 @@ import Calendar from "primevue/calendar";
 import Dialog from "primevue/dialog";
 import InlineMessage from "primevue/inlinemessage";
 //import InputNumber from 'primevue/inputnumber';
-import TabView from "primevue/tabview";
-import TabPanel from "primevue/tabpanel";
 import Dropdown from "primevue/dropdown";
 import FileUpload from "primevue/fileupload";
 import Checkbox from "primevue/checkbox";
-import Timeline from "primevue/timeline";
 import axios from "axios";
 import $ from "jquery";
 
@@ -1388,16 +1174,14 @@ import moment from "moment";
 export default {
   props: ["systemId"],
   components: {
+    RightSidePanel,
     InputText,
     Button,
     DataTable,
     Column,
     Calendar,
     Dialog,
-    TabView,
-    TabPanel,
     InlineMessage,
-    Timeline,
     //InputNumber,
     Dropdown,
     FileUpload,
